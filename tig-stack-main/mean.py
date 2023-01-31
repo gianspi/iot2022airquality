4fd6a1b1eada9a8084aec9a7a8e39b964a86f119
gianspi <72250901+gianspi@users.noreply.github.com>
2023-01-31 17:18:57 +0100

    Update mean.py

--- tig-stack-main/mean.py
+++ tig-stack-main/mean.py
@@ -8,16 +8,18 @@ FORECAST_MEASUREMENT = 'forecast'
 token = "LUaijeA_-hxGtLkz9axuiCVt51pgGPakizsI7wESL5QAe0vEbr7z1CUoK42Jj0s8lrKT6UWzDmi32hc9E8g-Tw--"
 BUCKET = '"Air_Quality"'
 org = "IoT_Team"
-client = InfluxDBClient(url="http://influxdb:8086", token=token, debug=False, org=org)
+client = InfluxDBClient(url="http://influxdb:8086",
+                        token=token, debug=False, org=org)
 query_api = client.query_api()
 
 
-for field in FIELDS :
-    query = ' from(bucket:' + BUCKET + ') ' \
-                ' |> range(start: -15m) ' \
-                ' |> filter(fn: (r) => r._measurement == ' + AIR_QUALITY + ') ' \
-                ' |> filter(fn: (r) => r["_field"] == "temp" or r["_field"] == "hum" or r["_field"] == "conc")' \
-                ' |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value") ' \
-                ' |> mean() '
+query = ' from(bucket:' + BUCKET + ') ' \
+        ' |> range(start: -15m) ' \
+        ' |> filter(fn: (r) => r._measurement == ' + AIR_QUALITY + ') ' \
+        ' |> filter(fn: (r) => r["_field"] == "temp" or r["_field"] == "hum" or r["_field"] == "conc")' \
+        ' |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value") ' \
+        ' |> mean() '
 
-    result = client.query_api().query_data_frame(query)
+result = client.query_api().query_data_frame(query)
+
+display(result)
