e8d209facfe5482e6facb191c68171612815fe26
Matteo Boccali <matteo.boccali.06@gmail.com>
2023-06-09 17:08:16 +0200

    timestamp solution

--- tig-stack-main/telegraf/data_forecasting.py
+++ tig-stack-main/telegraf/data_forecasting.py
@@ -171,7 +171,7 @@ def addOutTemp(line,  out_temp) :
 def printDatetime(line) :
         last_wp = line.rfind(" ")
         logging.info("\n")
-        logging.info(datetime.fromtimestamp(int(line[last_wp:])))
+        logging.info(datetime.fromtimestamp(line[last_wp:]))
         logging.info("\n")
 
 
